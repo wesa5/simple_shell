f8ede40aa28949e7a6d39fb3ad37a20357b0956a
wesa5 <99413671+wesa5@users.noreply.github.com>
2022-05-18 12:57:41 +0300

    path

--- find_path.c
+++ find_path.c
@@ -1,49 +1,94 @@
 #include "main.h"
-
 /**
- * find_path - searches in PATH the executable command file.
- * @command: Command.
- * Return: Nothing.
+ * path_cmd -  Search In $PATH for executable command
+ * @cmd: Parsed input
+ * Return: 0 on success or  1 on failure  0
  */
-
-char *find_path(char *command)
+int path_cmd(char **cmd)
 {
-	char *copy = NULL, *ruta = NULL, **env = environ;
-	char **directories = malloc(100), *directory = NULL;
-	struct stat st;
-	int count = 0, i = 0, j = 0;
+	char *path, *value, *cmd_path;
+	struct stat buf;
 
-	for (count = 0; env[count] != NULL; count++)
+	path = _getenv("PATH");
+	value = _strtok(path, ":");
+	while (value != NULL)
 	{
-		if ((_strncmp("PATH", env[count], 4)) == 0)
-			break;
-	}
-	copy = malloc(_strlen(env[count]) + 1);
-	_strcpy(copy, env[count]);
-	directory = strtok(copy, ":=");
-	while (directory != NULL)
-	{
-		directories[i] = malloc(_strlen(directory) + _strlen(command) + 2);
-		_strcpy(directories[i], directory);
-		_strcat(directories[i], "/");
-		_strcat(directories[i], command);
-		if (stat(directories[i], &st) == 0)
+		cmd_path = build(*cmd, value);
+		if (stat(cmd_path, &buf) == 0)
 		{
-			ruta = malloc(_strlen(directories[i]) + 1);
-			_strcpy(ruta, directories[i]);
-			for (j = 0; j <= i; j++)
-				free(directories[j]);
-			free(directories);
-			free(copy);
-			return (ruta);
+			*cmd = _strdup(cmd_path);
+			free(cmd_path);
+			free(path);
+			return (0);
 		}
-		i++;
-		directory = strtok(NULL, ":");
+		free(cmd_path);
+		value = _strtok(NULL, ":");
 	}
-	for (j = 0; j < i; j++)
-		free(directories[j]);
-	free(directories);
-	free(directory);
-	free(copy);
+	free(path);
+	free(value);
+	return (1);
+}
+/**
+ * build - Build command
+ * @token: Executable command
+ * @value: Directory conatining Command
+ * Return: Parsed full path of command or NULL if failed
+ */
+char *build(char *token, char *value)
+{
+	char *cmd;
+	size_t len;
+
+	len = _strlen(value) + _strlen(token) + 2;
+	cmd = malloc(sizeof(char) * len);
+	if (cmd == NULL)
+	{
+		free(cmd);
+		return (NULL);
+	}
+
+	memset(cmd, 0, len);
+
+	cmd = _strcat(cmd, value);
+	cmd = _strcat(cmd, "/");
+	cmd = _strcat(cmd, token);
+
+	return (cmd);
+}
+/**
+ * _getenv - Gets the value of environment variable by name
+ * @name: Environment variable name
+ * Return: The value of the environment variable or NULL if failed
+ */
+char *_getenv(char *name)
+{
+	size_t name_len, value_len;
+	char *value;
+	int i, j, k;
+
+	name_len = _strlen(name);
+	for (i = 0 ; environ[i]; i++)
+	{
+		if (_strncmp(name, environ[i], name_len) == 0)
+		{
+			value_len = _strlen(environ[i]) - name_len;
+			value = malloc(sizeof(char) * value_len);
+			if (!value)
+			{
+				free(value);
+				perror("unable to alloc");
+				return (NULL);
+			}
+
+			j = 0;
+			for (k = name_len + 1; environ[i][k]; k++, j++)
+			{
+				value[j] = environ[i][k];
+			}
+			value[j] = '\0';
+
+			return (value);
+		}
+	}
 	return (NULL);
 }
